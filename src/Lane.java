b58910460f767e6c39d7130315752903a33b8ceb
jkeelagher <joseph.keelagher@student.unimelb.edu.au>
2023-10-10 22:58:26 +1100

    Added Lane and NormalLane

--- src/Lane.java
+++ src/Lane.java
@@ -9,11 +9,34 @@ public abstract class Lane {
     protected final int location;
     protected final int currNote = 0;
 
+    public abstract String getType();
+
     public abstract int update(Input input, Accuracy accuracy);
     public abstract void addNote(Note note);
     public abstract boolean isFinished();
     public abstract void draw(int x, int y);
-    public abstract void clearLane();
+
+    public Lane(String type, int location) {
+        this.type = type;
+        this.location = location;
+        image = new Image("res/lane" + type + ".png");
+        switch (type) {
+            case "Left":
+                relevantKey = Keys.LEFT;
+                break;
+            case "Right":
+                relevantKey = Keys.RIGHT;
+                break;
+            case "Up":
+                relevantKey = Keys.UP;
+                break;
+            case "Down":
+                relevantKey = Keys.DOWN;
+                break;
+            case "Special":
+                relevantKey = Keys.SPACE;
+                break;
+        }
 
 
 }
--- src/NormalLane.java
+++ src/NormalLane.java
@@ -1,2 +1,82 @@
-public class NormalLane {
+import bagel.*;
+
+public class NormalLane extends Lane{
+    private final String type;
+    private int numHoldNotes = 0;
+    private int currHoldNote = 0;
+
+    public NormalLane(String type, int location) {
+        super(type, location);
+    }
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void addNote(Note note) {
+        notes[numNotes++] = n;
+    }
+
+    @Override
+    public boolean isFinished() {
+        for (int i = 0; i < numNotes; i++) {
+            if (!notes[i].isCompleted()) {
+                return false;
+            }
+        }
+
+        for (int j = 0; j < numHoldNotes; j++) {
+            if (!holdNotes[j].isCompleted()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void draw(int x, int y) {
+        image.draw(location, HEIGHT);
+
+        for (int i = currNote; i < numNotes; i++) {
+            notes[i].draw(location);
+        }
+
+        for (int j = currHoldNote; j < numHoldNotes; j++) {
+            holdNotes[j].draw(location);
+        }
+    }
+
+    @Override
+    public int update(Input input, Accuracy accuracy) {
+        draw();
+
+        for (int i = currNote; i < numNotes; i++) {
+            notes[i].update();
+        }
+
+        for (int j = currHoldNote; j < numHoldNotes; j++) {
+            holdNotes[j].update();
+        }
+
+        if (currNote < numNotes) {
+            int score = notes[currNote].checkScore(input, accuracy, TARGET_HEIGHT, relevantKey);
+            if (notes[currNote].isCompleted()) {
+                currNote++;
+                return score;
+            }
+        }
+
+        if (currHoldNote < numHoldNotes) {
+            int score = holdNotes[currHoldNote].checkScore(input, accuracy, TARGET_HEIGHT, relevantKey);
+            if (holdNotes[currHoldNote].isCompleted()) {
+                currHoldNote++;
+            }
+            return score;
+        }
+
+        return Accuracy.NOT_SCORED;
+    }
+
 }
